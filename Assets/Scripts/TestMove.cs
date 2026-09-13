f203166bc884e03d7fd77e7bb203785450dcbe9c
xtchhh <chappellxander77@gmail.com>
2026-09-13 02:29:12 -0400

    Attempting bike rotations

--- Assets/Scripts/TestMove.cs
+++ Assets/Scripts/TestMove.cs
@@ -5,9 +5,9 @@ using UnityEngine.InputSystem;
 public class TestMove : MonoBehaviour
 {
     public Camera cam;
-    public float moveSpeed;
     private CharacterController characterController;
     public InputSystem_Actions inputActions;
+    public float moveSpeed;
     private float gravity = -9.41f;
     private int health = 2;
     public int damage;
@@ -33,10 +33,12 @@ public class TestMove : MonoBehaviour
 
     void Movement()
     {
+        Vector3 Xinput = Vector3.zero;
+
         move = inputActions.Player.Move.ReadValue<Vector2>();
 
         Vector3 cameraForward = cam.transform.forward;
-        cameraForward.y = 0; //move horizontal
+        cameraForward.y = 0;
         float forwardInput = move.y;
 
         Vector3 forwardDirection = cameraForward.normalized * forwardInput;
@@ -44,10 +46,49 @@ public class TestMove : MonoBehaviour
 
         characterController.Move(direction * Time.deltaTime);
 
-        if (move.sqrMagnitude > 0.1)
+        if (Keyboard.current.dKey.isPressed)
         {
-            this.transform.rotation = Quaternion.LookRotation(forwardDirection);
+            Xinput += transform.InverseTransformDirection(Vector3.right);
         }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Xinput += transform.InverseTransformDirection(Vector3.left);
+        }
+        
+        BikeRotation(cameraForward, Xinput);
+        Debug.DrawLine(transform.position, transform.position + Xinput * 1.5f, Color.blue, 0.01f);
+    }
+
+    void BikeRotation(Vector3 dir, Vector3 rightDir)
+    {
+        Vector3 cameraRight = cam.transform.right;
+        cameraRight = cameraRight.normalized;
+        cameraRight.y = 0;
+
+        Vector3 rightDirection = cameraRight * rightDir.x;
+
+        float aroundZleft = rightDirection.x * -45.0f;
+
+        Quaternion orignalRotation = Quaternion.identity;
+        Quaternion currentRot = Quaternion.Euler(0, 0, 0);
+
+        Quaternion target = Quaternion.LookRotation(dir);
+        transform.rotation = target * currentRot;
+
+        if (rightDir.sqrMagnitude > 0.1)
+        {
+            Quaternion localRot = Quaternion.Euler(0, 0, aroundZleft);
+            currentRot = Quaternion.Slerp(transform.rotation, currentRot, Time.deltaTime * 5.0f);
+        }
+
+        if (rightDir.sqrMagnitude < 0.1)
+        {
+            Quaternion localRot = orignalRotation;
+            currentRot = Quaternion.Slerp(transform.rotation, currentRot, Time.deltaTime * 2.5f);
+        }
+
+        Debug.Log($"{rightDir.x} + {rightDir.sqrMagnitude}");
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
